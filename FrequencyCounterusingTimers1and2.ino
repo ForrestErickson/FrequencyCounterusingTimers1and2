513745cb4772548754e14231cf6aa9ddb69a15fa
Forrest Erickson <forresterickson@comcast.net>
2023-05-20 14:43:51 -0400

    Add serial input for PWM value. Not fault tollerent.

--- FrequencyCounterusingTimers1and2.ino
+++ FrequencyCounterusingTimers1and2.ino
@@ -1,8 +1,10 @@
 //====================================
 //Frequency Counter using Timers 1 & 2
 // Modified from code found here: https://akuzechie.blogspot.com/2021/02/frequency-counter-using-arduino-timers.html
+// Information on Arduino UNO counter use here: https://docs.arduino.cc/tutorials/generic/secrets-of-arduino-pwm
 //====================================
 // Input on pin D5
+// Output PWM to fan on D6
 //====================================
 
 #define PROG_NAME "**** FrequencyCounterusingTimers1and2 ****"
@@ -12,6 +14,11 @@
 
 #define PLOTTING true
 
+//For serial input
+String inputString = "";         // a String to hold incoming data
+bool stringComplete = false;  // whether the string is complete
+
+//For frequency counter
 volatile unsigned long totalCounts;
 volatile bool finishedCount;
 volatile unsigned long Timer1overflowCounts;
@@ -56,6 +63,16 @@ void loop()
 
   }
 
+  // print the string when a newline arrives:
+  if (stringComplete) {
+    Serial.println(inputString);
+    analogWrite(6, (255-inputString.toInt()));    //To Fan PWM.
+//    analogWrite(6, Serial.parseInt());    //To Fan PWM.
+    // clear the string:
+    inputString = "";
+    stringComplete = false;
+  }
+
 }
 //=================================================================
 void startCount(unsigned int period)
@@ -97,4 +114,23 @@ ISR (TIMER2_COMPA_vect)
 
   totalCounts = (overflowCounts * 65536) + TCNT1;
   finishedCount = true;
+}
+
+/*
+  SerialEvent occurs whenever a new data comes in the hardware serial RX. This
+  routine is run between each time loop() runs, so using delay inside loop can
+  delay response. Multiple bytes of data may be available.
+*/
+void serialEvent() {
+  while (Serial.available()) {
+    // get the new byte:
+    char inChar = (char)Serial.read();
+    // add it to the inputString:
+    inputString += inChar;
+    // if the incoming character is a newline, set a flag so the main loop can
+    // do something about it:
+    if (inChar == '\n') {
+      stringComplete = true;
+    }
+  }
 }

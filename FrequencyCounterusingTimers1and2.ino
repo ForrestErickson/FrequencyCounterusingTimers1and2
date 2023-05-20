e692b11638aae92e5eb03d854c9646f4e9bfd243
Forrest Erickson <forresterickson@comcast.net>
2023-05-20 12:10:34 -0400

    Remove LCD code. Change serial output for compaibility with Arduino IDE Serial Plotter.

--- FrequencyCounterusingTimers1and2.ino
+++ FrequencyCounterusingTimers1and2.ino
@@ -1,17 +1,13 @@
 //====================================
 //Frequency Counter using Timers 1 & 2
-// From: https://akuzechie.blogspot.com/2021/02/frequency-counter-using-arduino-timers.html
+// Modified from code found here: https://akuzechie.blogspot.com/2021/02/frequency-counter-using-arduino-timers.html
 //====================================
-
-//====================================
-//Frequency Counter using Timers 1 & 2
 // Input on pin D5
 //====================================
 #include <Wire.h>
-#include <Adafruit_SSD1306.h>
-//------------------------------------------
-Adafruit_SSD1306 display(128, 64, &Wire, 4);
-//------------------------------------------
+
+#define PLOTTING true
+
 volatile unsigned long totalCounts;
 volatile bool nextCount;
 volatile unsigned long Timer1overflowCounts;
@@ -21,25 +17,29 @@ unsigned int counter, countPeriod;
 void setup()
 {
   Serial.begin(112500);
-  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
-  display.clearDisplay(); 
+  delay(100);
+  Serial.print("RPM,");
   analogReference(INTERNAL);
+  //  Make a single read
+  startCount(1000);
+  while (!nextCount) {}
 }
 //=================================================================
 void loop()
 {
   startCount(1000);
-  while(!nextCount) {}
+  while (!nextCount) {}
+#ifndef PLOTTING
+  Serial.print("A0=: ");
+  Serial.print(analogRead(A0));
+  Serial.print(", ");
+  Serial.print("Frequency (Hz): ");
+  Serial.print(totalCounts);
+  Serial.print(", RMP: ");
+#endif  
+  Serial.print(totalCounts * 30);
+  Serial.println();
 
-  display.setTextColor(WHITE); display.clearDisplay();
-  display.setTextSize(2);
-  display.setCursor(5,0); display.print("Frequency");
-  display.setCursor(0,18); display.print("=== Hz ===");
-  display.setTextSize(3); display.setCursor(0,42);
-  display.print(totalCounts);
-  Serial.print("Total counts ");
-  Serial.println(totalCounts);
-  display.display();
 }
 //=================================================================
 void startCount(unsigned int period)
@@ -48,7 +48,7 @@ void startCount(unsigned int period)
   counter = 0;
   Timer1overflowCounts = 0;
   countPeriod = period;
-  
+
   //Timer 1: overflow interrupt due to rising edge pulses on D5
   //Timer 2: compare match interrupt every 1ms
   noInterrupts();
@@ -73,10 +73,10 @@ ISR (TIMER2_COMPA_vect)
 {
   overflowCounts = Timer1overflowCounts;
   counter++;
-  if(counter < countPeriod) return;
+  if (counter < countPeriod) return;
 
-  TCCR1A = 0; TCCR1B = 0;   //Timer 1 reset  
-  TCCR2A = 0; TCCR2B = 0;   //Timer 2 reset    
+  TCCR1A = 0; TCCR1B = 0;   //Timer 1 reset
+  TCCR2A = 0; TCCR2B = 0;   //Timer 2 reset
   TIMSK1 = 0; TIMSK2 = 0;   //Timer 1 & 2 disable interrupts
 
   totalCounts = (overflowCounts * 65536) + TCNT1;

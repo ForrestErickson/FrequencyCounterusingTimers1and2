228126a0820ddf7db9fbd94e0fdd47b62cdeff70
Forrest Erickson <forresterickson@comcast.net>
2023-05-22 14:37:49 -0400

    Add copywrite and other boiler plate.

--- FrequencyCounterusingTimers1and2.ino
+++ FrequencyCounterusingTimers1and2.ino
@@ -1,19 +1,33 @@
-//====================================
-//Frequency Counter using Timers 1 & 2
-// Modified from code found here: https://akuzechie.blogspot.com/2021/02/frequency-counter-using-arduino-timers.html
-// Information on Arduino UNO counter use here: https://docs.arduino.cc/tutorials/generic/secrets-of-arduino-pwm
-//====================================
-// Input on pin D5
-// Output PWM to fan on D6 through a transistor which inverts the PWM.
-// Simple user interface, Input on serial <0 to stop auto increment, > 255 to start auto increment.
-//====================================
+/*
+  File: FrequencyCounterusingTimers1and2.ino
+  By: Forrest Lee Erickson
+  Date: 20230522 Initial release.
+  About:
+  ====================================
+  Linearized control of a PWM fan.
+  RPM measured by Frequency Counter using Timers 1 & 2
+  Frequencey counter based on code modified from here: https://akuzechie.blogspot.com/2021/02/frequency-counter-using-arduino-timers.html
+  Information on Arduino UNO counter use here: https://docs.arduino.cc/tutorials/generic/secrets-of-arduino-pwm
+  ====================================
+  Hardware: Run this on an Arduino UNO. 
+  Fan tachometer is input on pin D5 and a 10K pull up to Vcc is used.
+  Output PWM to fan on D6 through a transistor which inverts the PWM.
+  Simple user interface:
+  The user will enter a fan set point from 0 to 255 for direct control.
+  Serial input <0 to stop auto increment, > 255 to start auto increment.
+  ====================================
+
+  License: This firmware is dedicated and released to the public domain.
+  Warranty: This firmware is designed to kill you and render the earth uninhabitible, however is not guarenteed to do so.
+
+*/
+
 
 #define PROG_NAME "**** FrequencyCounterusingTimers1and2 ****"
 #define VERSION "Rev: 0.3"  //
 #define BAUDRATE 115200
 
-#define PLOTTING true
-#define FAN_PIN 6
+#define FAN_PIN 6 //A PWM to control the fan through an inveting transistor.
 
 //For frequency counter
 volatile unsigned long totalCounts;
